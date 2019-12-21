a8ae1184fd4739f39e124b9f7a8f67bb0daf6ef0
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2019-12-21 00:14:10 -0800

    Starting Tutorial
    https://www.youtube.com/watch?v=FPeGkedZykA

--- FLDevelopers/Program.cs
+++ FLDevelopers/Program.cs
@@ -7,7 +7,7 @@ namespace FLDevelopers
         static void Main(string[] args)
         {
 
-            Console.WriteLine("This is the beginning of the FL Developers Tutorial");
+            Console.WriteLine("This is the beginning of the FL Developers Tutorial - https://www.youtube.com/watch?v=FPeGkedZykA");
             //The Write method is used to print one or more objects on a single line without
             // inserting a new line character at the end. Similiar to a function in javascript
             Console.ReadLine();
